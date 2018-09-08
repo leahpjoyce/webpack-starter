6d6dc8841d9100d30a321d13bb2127120a102f5e
Leah Joyce <leahpjoyce@gmail.com>
2018-09-08 00:09:52 -0500

    My playground

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!!!');
+let person = {
+  name: "John",
+  age: 32,
+  partTime: false
+};
+console.log(person['name']); // John
+console.log(person['age']); // 32
+console.log(person['partTime']); // false
